24da28679e30fc842ffdad385e9189127ab6d458
pr-Dutta <89duttapranjal@gmail.com>
2024-10-28 19:27:07 +0530

    new learning

--- app/src/main/java/com/example/expensetrackerapp/presentation/ExpenseEvent.kt
+++ app/src/main/java/com/example/expensetrackerapp/presentation/ExpenseEvent.kt
@@ -2,12 +2,12 @@ package com.example.expensetrackerapp.presentation
 
 import com.example.expensetrackerapp.data.Expense
 
-//sealed interface ExpenseEvent {
-//    object SortExpense: ExpenseEvent
-//    data class DeleteExpense(val expense: Expense): ExpenseEvent
-//    data class SaveExpense(
-//        val title: String,
-//        val amount: Int,
-//        val category: String
-//    ): ExpenseEvent
-//}
+sealed interface ExpenseEvent {
+    object SortExpense: ExpenseEvent
+    data class DeleteExpense(val expense: Expense): ExpenseEvent
+    data class SaveExpense(
+        val title: String,
+        val amount: Int,
+        val category: String
+    ): ExpenseEvent
+}
