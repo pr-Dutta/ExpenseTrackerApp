db277c9055610e2d1c3d891e777ac2b61191f3b5
pr-Dutta <89duttapranjal@gmail.com>
2024-10-30 12:03:24 +0530

    new learning

--- app/src/main/java/com/example/expensetrackerapp/presentation/ExpenseState.kt
+++ app/src/main/java/com/example/expensetrackerapp/presentation/ExpenseState.kt
@@ -5,9 +5,9 @@ import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import com.example.expensetrackerapp.data.Expense
 
-//data class ExpenseState(
-//    val expenseList: List<Expense> = emptyList(),
-//    val title: MutableState<String> = mutableStateOf(""),
-//    val amount: MutableState<Int> = mutableIntStateOf(0),
-//    val category: MutableState<String> = mutableStateOf("")
-//)
+data class ExpenseState(
+    val expenseList: List<Expense> = emptyList(),
+    val title: MutableState<String> = mutableStateOf(""),
+    val amount: MutableState<Int> = mutableIntStateOf(0),
+    val category: MutableState<String> = mutableStateOf("")
+)
